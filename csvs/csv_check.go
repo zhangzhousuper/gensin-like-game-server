e649bf6291046de009b815a118f997071b9fb658
zhangzhousuper <1063052346@qq.com>
2022-04-10 15:46:51 +0800

    can chouka

--- csvs/csv_check.go
+++ csvs/csv_check.go
@@ -1,15 +1,19 @@
 package csvs
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 var (
 	ConfigDropGroupMap map[int]*DropGroup
 )
 
 type DropGroup struct {
-	DropId     int
-	WeightAll  int
-	DropConfig []*ConfigDrop
+	DropId      int
+	WeightAll   int
+	DropConfigs []*ConfigDrop
 }
 
 func CheckLoadCsv() {
@@ -28,7 +32,47 @@ func MakeDropGroupMap() {
 			ConfigDropGroupMap[v.DropId] = dropGroup
 		}
 		dropGroup.WeightAll += v.Weight
-		dropGroup.DropConfig = append(dropGroup.DropConfig, v)
+		dropGroup.DropConfigs = append(dropGroup.DropConfigs, v)
 	}
+	RunDropTest()
 	return
+}
+
+func RunDropTest() {
+	dropGroup := ConfigDropGroupMap[1000]
+	if dropGroup == nil {
+		return
+	}
+
+	num := 0
+	for {
+		config := GetRandDrop(dropGroup)
+		if config.IsEnd == LOGIC_TRUE {
+			fmt.Println(GetItemName(config.Result))
+			num++
+			dropGroup = ConfigDropGroupMap[1000]
+			if num >= 100 {
+				break
+			} else {
+				continue
+			}
+		}
+		dropGroup = ConfigDropGroupMap[config.Result]
+		if dropGroup == nil {
+			break
+		}
+	}
+}
+
+func GetRandDrop(dropGroup *DropGroup) *ConfigDrop {
+	rand.Seed(time.Now().Unix())
+	randNum := rand.Intn(dropGroup.WeightAll)
+	randNow := 0
+	for _, v := range dropGroup.DropConfigs {
+		randNow += v.Weight
+		if randNum < randNow {
+			return v
+		}
+	}
+	return nil
 }
