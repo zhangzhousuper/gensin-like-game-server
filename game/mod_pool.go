15ae15c9e0918eab54eb19fea917daf370f2c566
zhangzhousuper <1063052346@qq.com>
2022-04-10 23:41:23 +0800

    small test

--- game/mod_pool.go
+++ game/mod_pool.go
@@ -8,6 +8,8 @@ import (
 type PoolInfo struct {
 	PoolId        int
 	FiveStarTimes int
+	FourStarTimes int
+	IsMustUp      int
 }
 
 type ModPool struct {
@@ -16,18 +18,27 @@ type ModPool struct {
 
 func (self *ModPool) DoUpPool() {
 	result := make(map[int]int)
+	fourNum := 0
+	fiveNum := 0
+	resultEach := make(map[int]int)
 	for i := 0; i < 10000000; i++ {
 		dropGroup := csvs.ConfigDropGroupMap[1000]
 		if dropGroup == nil {
 			return
 		}
 
-		if self.UpPoolInfo.FiveStarTimes > csvs.FIVE_STAR_TIMES_LIMIT {
+		if self.UpPoolInfo.FiveStarTimes > csvs.FIVE_STAR_TIMES_LIMIT || self.UpPoolInfo.FourStarTimes > csvs.FOUR_STAR_TIMES_LIMIT {
 			newDropGroup := new(csvs.DropGroup)
 			newDropGroup.DropId = dropGroup.DropId
 			newDropGroup.WeightAll = dropGroup.WeightAll
 			addFiveWeight := (self.UpPoolInfo.FiveStarTimes - csvs.FIVE_STAR_TIMES_LIMIT) * csvs.FIVE_STAR_TIMES_LIMIT_EACH_VALUE
-
+			if addFiveWeight < 0 {
+				addFiveWeight = 0
+			}
+			addFourWeight := (self.UpPoolInfo.FourStarTimes - csvs.FOUR_STAR_TIMES_LIMIT) * csvs.FOUR_STAR_TIMES_LIMIT_EACH_VALUE
+			if addFourWeight < 0 {
+				addFourWeight = 0
+			}
 			for _, config := range dropGroup.DropConfigs {
 				newConfig := new(csvs.ConfigDrop)
 				newConfig.Result = config.Result
@@ -35,8 +46,10 @@ func (self *ModPool) DoUpPool() {
 				newConfig.IsEnd = config.IsEnd
 				if config.Result == 10001 {
 					newConfig.Weight = config.Weight + addFiveWeight
+				} else if config.Result == 10002 {
+					newConfig.Weight = config.Weight + addFourWeight
 				} else if config.Result == 10003 {
-					newConfig.Weight = config.Weight
+					newConfig.Weight = config.Weight - addFiveWeight - addFourWeight
 				}
 				newDropGroup.DropConfigs = append(newDropGroup.DropConfigs, newConfig)
 				dropGroup = newDropGroup
@@ -46,11 +59,38 @@ func (self *ModPool) DoUpPool() {
 		roleIdConfig := csvs.GetRandDropNew(dropGroup)
 		if roleIdConfig != nil {
 			roleConfig := csvs.GetRoleConfig(roleIdConfig.Result)
-			if roleConfig != nil && roleConfig.Star == 5 {
-				// 抽中五星后
-				self.UpPoolInfo.FiveStarTimes = 0
+			if roleConfig != nil {
+				if roleConfig.Star == 5 {
+					resultEach[self.UpPoolInfo.FiveStarTimes]++
+					// 抽中五星后
+					self.UpPoolInfo.FiveStarTimes = 0
+					self.UpPoolInfo.FourStarTimes++
+					fiveNum++
+					// 大保底
+					if self.UpPoolInfo.IsMustUp == csvs.LOGIC_TRUE {
+						dropGroup := csvs.ConfigDropGroupMap[100012]
+						if dropGroup != nil {
+							roleIdConfig = csvs.GetRandDropNew(dropGroup)
+							if roleIdConfig == nil {
+								fmt.Println("数据异常")
+								return
+							}
+						}
+					}
+					if roleIdConfig.DropId == 100012 {
+						self.UpPoolInfo.IsMustUp = csvs.LOGIC_FALSE
+					} else {
+						self.UpPoolInfo.IsMustUp = csvs.LOGIC_TRUE
+					}
+
+				} else if roleConfig.Star == 4 {
+					self.UpPoolInfo.FourStarTimes = 0
+					self.UpPoolInfo.FourStarTimes++
+					fourNum++
+				}
 			} else {
 				self.UpPoolInfo.FiveStarTimes++
+				self.UpPoolInfo.FourStarTimes++
 			}
 			result[roleIdConfig.Result]++
 		}
@@ -58,5 +98,11 @@ func (self *ModPool) DoUpPool() {
 
 	for k, v := range result {
 		fmt.Println(fmt.Sprintf("抽中%s次数：%d", csvs.GetItemName(k), v))
+	}
+	fmt.Println(fmt.Sprintf("抽中4星：%d", fourNum))
+	fmt.Println(fmt.Sprintf("抽中5星：%d", fiveNum))
+
+	for k, v := range resultEach {
+		fmt.Println(fmt.Sprintf("第%d抽抽中五星的概率:%d", k, v))
 	}
 }
